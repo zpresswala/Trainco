7feb3043e55f74380ab83cfd9da3ec83b7453a10
Joe Saperstein <joe.saperstein@imulus.com>
2015-06-19 15:40:49 -0600

    setup continued, gulp working w/sass and sever

--- gulpfile.js
+++ gulpfile.js
@@ -5,28 +5,49 @@ var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     sass = require('gulp-sass'),
     watch = require('gulp-watch'),
-    serverPort = 3000,
-    server;
+    webserver = require('gulp-webserver'),
+    serverPort = 3000;
 
+// set the paths for css compiling
 var config = {
-	sassPath: 'src/TPCTrainco.Umbraco/static/bower_components/bootstrap-sass/assets/stylesheets',
-	sassDest: 'src/TPCTrainco.Umbraco/assets/stylesheets'
+	sassPath: 'src/TPCTrainco.Umbraco/assets/bootstrap-sass/assets/stylesheets',
+    sassPathCustom: 'src/TPCTrainco.Umbraco/assets/scss',
+	sassDest: 'src/TPCTrainco.Umbraco/css',
+    htmlPath: 'src/TPCTrainco.Umbraco/'
 };
 
-gulp.task('watch', function () {
-	console.log('watch')
-   gulp.watch(config.sassPath + '/**/*.scss', function() {
-   		gulp.run('sass');
-   });
-});
-
+// gulp sass task, compiling all bootstrap sass files and our custom sass files
 gulp.task('sass', function() {
-	gulp.src(config.sassPath + '/**/*.scss')
+    gulp.src([config.sassPath + '/**/*.scss', config.sassPathCustom + '/*.scss'])
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest(config.sassDest));
 });
 
-
-gulp.task('default',function() {
-    gulp.watch(config.sassPath + '/**/*.scss',['sass']);
+gulp.task('webserver', function() {
+    gulp.src(config.htmlPath)
+        .pipe(webserver({
+            // root: '/src',
+            livereload: true,
+            // directoryListing: true,
+            open: true,
+            port: 3000
+    }));
 });
+
+// watching all bootstrap sass files and our custom sass files
+gulp.task('watch', function () {
+    gulp.watch([config.sassPath + '/**/*.scss', config.sassPathCustom + '/*.scss'], ['sass']);
+    // js watch call goes here
+});
+
+
+// gulp.task('default',function() {
+//     gulp.watch(config.sassPath + '/**/*.scss',['sass']);
+// });
+
+gulp.task('default', ['sass', 'webserver', 'watch']);
+
+// need: 
+// css, js concatenate
+// css, js minify
+// image optim
