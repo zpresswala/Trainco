b9e4db5bb9960336ba39872e2f017633682a4575
Joe Saperstein <joe.saperstein@imulus.com>
2015-06-24 15:47:49 -0600

    everything in, css touch-ups now

--- src/TPCTrainco.Umbraco/assets/js/HomePage.js
+++ src/TPCTrainco.Umbraco/assets/js/HomePage.js
@@ -1,24 +1,20 @@
 'use strict';
 
 function HomePage() {
-	this.$overlay = $('.overlay');
+	this.$overlay = $('.overlay-contain');
 
 	this.showActiveSelection();
 }
 
 HomePage.prototype.showActiveSelection = function() {
 	// console.log(this.$overlay);
-	// $('.seminar-topic').click(function() { 
-	//     var $ths = $(this);
-	//     if($ths.hasClass('chosen'))
-	//         $ths.removeClass('chosen');
-	//     else
-	//         $ths.addClass('chosen-click');
-	// }).mouseleave(function() {
-	// 	if(!$(this).hasClass('chosen-click')) {
-	// 		$(this).removeClass('chosen');
-	// 	}
-	// }).mouseenter(function() { 
-	//     $(this).addClass('chosen');
-	// });
+	this.$overlay.on('click', function() { 
+	    var $this = $(this);
+	    if($this.hasClass('chosen')) {
+	    	$this.removeClass('chosen');
+	    }
+	 	else {
+	 		$this.addClass('chosen');
+	 	}
+	});
 };
