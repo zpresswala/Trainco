c3d50627325b30144f3e690c7f7cba7b1ae0097e
John Skufca <john.skufca@imulus.com>
2015-06-26 09:58:41 -0600

    Initial Install and Homepage Integration

---
+++ src/TPCTrainco.Umbraco.Extensions/Helpers/Nodes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace TPCTrainco.Umbraco.Extensions.Helpers
+{
+    public sealed class Nodes
+    {
+        private static volatile Nodes instance;
+        private static object _padLock = new Object();
+
+        private Nodes() { }
+
+        public IPublishedContent Home;
+        public IPublishedContent SiteSettings;
+        public IPublishedContent News;
+        public IPublishedContent RedirectFolder;
+        public IEnumerable<IPublishedContent> Redirects;
+        public IPublishedContent SocialLinkFolder;
+        public IEnumerable<IPublishedContent> SocialLinks;
+
+        public UmbracoHelper UmbracoHelper;
+
+        public static Nodes Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_padLock)
+                    {
+                        if (instance == null)
+                        {
+                            var umbHelper = new UmbracoHelper(UmbracoContext.Current);
+
+                            instance = new Nodes();
+                            instance.UmbracoHelper = umbHelper;
+
+                            instance.Home = umbHelper.TypedContentAtRoot().FirstOrDefault(n => n.IsDocumentType("HomePage"));
+                            instance.SiteSettings = umbHelper.TypedContentAtRoot().FirstOrDefault(n => n.IsDocumentType("SiteSettings"));
+                            instance.RedirectFolder = instance.SiteSettings.Children.FirstOrDefault(n => n.IsDocumentType("RedirectsFolder"));
+                            instance.Redirects = instance.RedirectFolder.Children;
+
+                            instance.SocialLinkFolder = instance.SiteSettings.Children.FirstOrDefault(n => n.IsDocumentType("SocialLinks"));
+                            instance.SocialLinks = instance.SocialLinkFolder.Children;
+                        }
+                    }
+                }
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+    }
+}
