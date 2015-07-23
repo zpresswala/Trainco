9e7e491fa47f80d2860083784ac5e6c79fe98a5a
Joe Saperstein <joe.saperstein@imulus.com>
2015-07-23 14:38:04 -0600

    search autofill done

--- src/TPCTrainco.Umbraco/assets/js/custom/MainSearchSelect.js
+++ src/TPCTrainco.Umbraco/assets/js/custom/MainSearchSelect.js
@@ -2,21 +2,38 @@
 
 function MainSearchSelect() {
 
-	// $.fn.select2.defaults.set("theme", "classic");
+	var sampleArray = [
+						{	
+							id: 0,
+							text:'enhancement'
+						}, 
 
-	var sampleArray = [{id:0,text:'enhancement'}, {id:1,text:'bug'}
-	                       ,{id:2,text:'duplicate'},{id:3,text:'invalid'}
-	                       ,{id:4,text:'wontfix'}]
+						{	
+							id:1,
+							text:'bug'
+						},
+						{
+							id:2,
+							text:'duplicate'
+						},
+						{
+							id:3,
+							text:'invalid'
+						},
+	                    {
+	                    	id:4,
+	                    	text:'wontfix'
+	                    }
+	                ];
 
 
 	$('#main-search').select2({
-
-		tags: true,
-		tokenSeparators: [',', ' '],
 		data:sampleArray,
-		// minimumResultsForSearch: -1,
+		tags: true,	
 		selectOnBlur: true,
 		maximumSelectionLength: 1,
+		dropdownAutoWidth: true,
+
 		placeholder: function() {
 			$(this).data('placeholder');
 		},
@@ -44,12 +61,11 @@ function MainSearchSelect() {
 		// 	alert('stuff')
 		// 	list.splice(1, 0, item);
 		// }
-	})
-	.on('change', function(e) {
-		var isNew = $(this).find('[data-select2-tag="true"]');
-		console.log(isNew)
-		if(isNew.length){
-			isNew.replaceWith('<option class="user-added" selected value="'+isNew.val()+'">'+isNew.val()+'</option>');
-		}
 	});
+	// .on('change', function(e) {
+		// var isNew = $(this).find('[data-select2-tag="true"]');
+		// if(isNew.length){
+		// 	isNew.replaceWith('<option class="selected value="'+isNew.val()+'">'+isNew.val()+'</option>');
+		// }
+	// });
 };
