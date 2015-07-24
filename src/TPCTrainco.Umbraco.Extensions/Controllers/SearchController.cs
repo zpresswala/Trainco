048ce32058a9ea8818718c252a9209ace80765f3
John Skufca <john.skufca@imulus.com>
2015-07-23 22:17:09 -0600

    Umbraco Upgrade to 7.2.8

---
+++ src/TPCTrainco.Umbraco.Extensions/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace TPCTrainco.Umbraco.Extensions.Controllers
+{
+    public class SearchController : ApiController
+    {
+        [HttpGet]
+        public object Index()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
