30490f6a1a513a4145f8dd03a3038068d2ac9ff0
John Skufca <john.skufca@imulus.com>
2015-07-30 01:30:55 -0600

    camelCase JSON Output

--- src/TPCTrainco.Umbraco/App_Start/WebApiConfig.cs
+++ src/TPCTrainco.Umbraco/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
 
@@ -11,6 +13,10 @@ namespace TPCTrainco.Umbraco
         public static void Register(HttpConfiguration config)
         {
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+
+            // Serialize with camelCase formatter for JSON.
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
