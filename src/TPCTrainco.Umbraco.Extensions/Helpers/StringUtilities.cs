e20fc6814e498f5a0165c50a933722b4c35bbcfd
John Skufca <john.skufca@imulus.com>
2015-08-20 08:39:55 -0600

    Added Contact Template and Wired Up City/State Dropdown

---
+++ src/TPCTrainco.Umbraco.Extensions/Helpers/StringUtilities.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCTrainco.Umbraco.Extensions.Helpers
+{
+    public class StringUtilities
+    {
+        public static string LineBreaksToParagraphs(string text)
+        {
+            if (false == string.IsNullOrWhiteSpace(text))
+            {
+                List<string> breakArray = text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (breakArray != null && breakArray.Count > 0)
+                {
+                    text = "";
+
+                    foreach (string line in breakArray)
+                    {
+                        text += "<p>" + line + "</p>\n\r";
+                    }
+                }
+            }
+
+            return text;
+        }
+
+
+        public static string LineBreaksToEmailList(string text)
+        {
+            if (false == string.IsNullOrWhiteSpace(text))
+            {
+                List<string> breakArray = text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (breakArray != null && breakArray.Count > 0)
+                {
+                    text = "";
+
+                    foreach (string line in breakArray)
+                    {
+                        text += "<li><a href=\"mailto:" + line + "\">" + line + "</a></li>\n\r";
+                    }
+                }
+            }
+
+            return text;
+        }
+    }
+}
---
+++ src/TPCTrainco.Umbraco.Extensions/Objects/Locations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoreLinq;
+
+namespace TPCTrainco.Umbraco.Extensions.Objects
+{
+    public class Locations
+    {
+        public List<Seminar_Catalog> SeminarList = null;
+
+
+        public Locations()
+        {
+            SeminarList = CacheObjects.GetSeminarList();
+        }
+
+        public List<ViewModels.Location> GetAllLocations()
+        {
+            List<ViewModels.Location> locations = null;
+
+            if (SeminarList != null && SeminarList.Count > 0)
+            {
+                locations = new List<ViewModels.Location>();
+
+                foreach (Seminar_Catalog seminarCatalog in SeminarList)
+                {
+                    ViewModels.Location location = new ViewModels.Location();
+
+                    location.CityState = seminarCatalog.City + ", " + seminarCatalog.State;
+                    locations.Add(location);
+                }
+            }
+
+            if (locations != null && locations.Count > 0)
+            {
+                locations = locations.DistinctBy(p => p.CityState).OrderBy(p => p.CityState).ToList();
+            }
+
+            return locations;
+        }
+    }
+}
