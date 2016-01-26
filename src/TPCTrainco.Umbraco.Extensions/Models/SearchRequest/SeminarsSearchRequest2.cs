4e1337fe726dedb68aecfb68f16621572863414c
John Skufca <john.skufca@imulus.com>
2016-01-26 13:39:17 -0700

    Getting Ready for New API Endpoint

---
+++ src/TPCTrainco.Umbraco.Extensions/Models/SearchRequest/SeminarsSearchRequest2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPCTrainco.Umbraco.Extensions.Models.SearchRequest
+{
+    public class SeminarsSearchRequest2
+    {
+        public string Location { get; set; }
+        public string Keywords { get; set; }
+        public int Radius { get; set; }
+        public string[] Topics { get; set; }
+        public DateRange DateRage { get; set; }
+        public int Page { get; set; }
+    }
+
+    public class DateRange
+    {
+        public DateTime Min { get; set; }
+        public DateTime Max { get; set; }
+    }
+}
