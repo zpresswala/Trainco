8841acca2b062200c26393c7bd71091bb7012d25
Steven Truesdell <steven@strues.io>
2016-01-29 09:34:00 -0700

    additional api notes

---
+++ dev/app/register/register.controller.js
@@ -0,0 +1,15 @@
+export class RegisterController {
+  constructor ($log) {
+    'ngInject';
+
+    this.$log = $log;
+
+    this.courseId = {};
+    this.activate();
+  }
+
+  activate() {
+
+  }
+
+}
