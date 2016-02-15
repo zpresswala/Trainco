c81efec8dd6376d78972dfb15621ff2b7189fdd6
Steven Truesdell <steven@strues.io>
2016-02-15 10:45:54 -0700

    using CDN for all libraries EXCEPT angular-select2.min.js

--- gulp/build.js
+++ gulp/build.js
@@ -19,7 +19,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'gulpang',
+      module: 'train',
       root: 'app'
     }))
     .pipe(gulp.dest(conf.paths.tmp + '/partials/'));
