0c57fe0767a193886158c6ed423d6dfbf03355e5
Steven Truesdell <steven@strues.io>
2016-02-16 11:11:33 -0700

    update url to relatives.

--- dev/app/index.route.js
+++ dev/app/index.route.js
@@ -10,17 +10,17 @@
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'app/main/main.html'
+        templateUrl: '/app/main/main.html'
       })
       .state('seminar', {
         url: '/seminar',
-        templateUrl: 'app/seminar/seminar.html',
+        templateUrl: '/app/seminar/seminar.html',
         controller: 'SeminarController',
         controllerAs: 'seminar'
       })
       .state('detail', {
         url: '/seminar/:id',
-        templateUrl: 'app/seminar/detail/detail.html',
+        templateUrl: '/app/seminar/detail/detail.html',
         controller: 'SeminarDetailController',
         controllerAs: 'detail',
 
@@ -36,16 +36,16 @@
         url: '/register',
         controller: 'RegisterController',
         controllerAs: 'register',
-        templateUrl: 'app/register/register.html'
+        templateUrl: '/app/register/register.html'
       })
       .state('empty', {
         url: '/results',
-        templateUrl: 'app/register/results/empty.html',
+        templateUrl: '/app/register/results/empty.html',
         parent: 'register'
       })
       .state('results', {
         url: '/results',
-        templateUrl: 'app/register/results/results.html',
+        templateUrl: '/app/register/results/results.html',
         parent: 'register'
       })
 
