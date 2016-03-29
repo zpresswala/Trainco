a286981cbbd9ba8d83e96e6f8ce2bad8f2e9d1d4
jason.thomas@axial.agency <jason.thomas@axial.agency>
2016-03-28 19:50:42 -0600

    Added custom content finder

---
+++ src/TPCTrainco.Umbraco.Extensions/Events/UmbracoApplicationStartUp.cs
@@ -0,0 +1,65 @@
+﻿using TPCTrainco.Umbraco.Extensions.Pipeline;
+using System.Web.Routing;
+using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+using Umbraco.Web.Routing;
+
+namespace TPCTrainco.Umbraco.Extensions.Events
+{
+    public class UmbracoApplicationStartUp : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            // Implemeting a Dashboard finder.
+            ContentFinderResolver.Current.InsertTypeBefore<ContentFinderByNiceUrl, DashbaordContentFinder>();
+
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+        }
+
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentService.Created += ContentCreated;
+
+            RegisterRoutes(RouteTable.Routes);
+
+            base.ApplicationStarted(umbracoApplication, applicationContext);
+        }
+
+        #region Event Handler Methods
+
+        /// <summary>
+        /// This is an event handler for inserting defualt values into new nodes.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContentCreated(IContentService sender, NewEventArgs<IContent> e)
+        {
+            //if (e.Entity.ContentType.Alias == "MyDocumentType")
+            //{
+            //	e.Entity.SetValue("pageTitle", "Untitled");
+            //	sender.Save(e.Entity);
+            //}
+        }
+
+
+        /// <summary>
+        /// Custom MVC Routes
+        /// </summary>
+        /// <param name="routes"></param>
+        private void RegisterRoutes(RouteCollection routes)
+        {
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //routes.MapRoute(
+            //	name: "Default",
+            //	url: "{controller}/{action}/{id}",
+            //	defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+        }
+
+        #endregion
+    }
+}
---
+++ src/TPCTrainco.Umbraco.Extensions/Pipeline/DashbaordContentFinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Web.Routing;
+
+namespace TPCTrainco.Umbraco.Extensions.Pipeline
+{
+    public class DashbaordContentFinder : IContentFinder
+    {
+        public bool TryFindContent(PublishedContentRequest contentRequest)
+        {
+            // We are looking for URLs of the form /dashboard/*
+            var urlParts = contentRequest.Uri.GetAbsolutePathDecoded().Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            if (urlParts.Length > 1 && urlParts[0].ToLower().Equals("dashboard"))
+            {
+                // Check to see if request is below the ad landing page, if so do silent redirect to it passing the categories
+                var dashboard = "/" + string.Join("/", urlParts[0]);
+                var dashboardContent = contentRequest.RoutingContext.UmbracoContext.ContentCache.GetByRoute(dashboard);
+
+                contentRequest.PublishedContent = dashboardContent;
+            }
+
+            return contentRequest.HasPublishedContent;
+        }
+    }
+}
+
