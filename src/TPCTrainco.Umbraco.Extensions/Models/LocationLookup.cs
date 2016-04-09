20c3c87b6d06f10c80a4c62c431871938f1c5044
John Skufca <john.skufca@imulus.com>
2016-04-08 21:23:03 -0600

    Create Location Lookup Table

--- src/TPCTrainco.Umbraco.Extensions/Models/LocationLookup.cs
+++ src/TPCTrainco.Umbraco.Extensions/Models/LocationLookup.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity.Spatial;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
 
 namespace TPCTrainco.Umbraco.Extensions.Models
 {
-    public class CoordinateDetails
+    [TableName("CacheLocationLookup")]
+    public class LocationLookup
     {
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
-        public DbGeography DbGeography { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string StateCode { get; set; }
-        public string Debug { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
