010c7d08189bd4f8f16113557b8343707e3e6f03
John Skufca <john.skufca@imulus.com>
2016-04-13 23:52:40 -0600

    Update Summary Checkout Page

--- src/TPCTrainco.Umbraco/assets/js/checkout2/summary.js
+++ src/TPCTrainco.Umbraco/assets/js/checkout2/summary.js
@@ -1,3 +1,20 @@
 ﻿$(document).ready(function () {
-    
+    CreateAccount();
 });
+
+function CreateAccount() {
+    var $speedCheckout = $('#speedCheckout');
+    var $speedCheckoutForm = $('.speedCheckout-form');
+    var $CreateAccount = $('#CreateAccount');
+
+    $speedCheckout.on('change', function () {
+        if ($(this).is(':checked')) {
+            $speedCheckoutForm.slideDown('fast');
+            $CreateAccount.val('true');
+
+        } else {
+            $speedCheckoutForm.slideUp('fast');
+            $CreateAccount.val('false');
+        }
+    });
+}
