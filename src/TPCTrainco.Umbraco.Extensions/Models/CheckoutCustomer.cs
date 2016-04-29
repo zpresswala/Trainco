2be18b58d379ba59f72419e580f1edc146f96522
John Skufca <john.skufca@imulus.com>
2016-04-29 10:37:38 -0600

    Fix Billing Email RegEx Validation

--- src/TPCTrainco.Umbraco.Extensions/Models/CheckoutCustomer.cs
+++ src/TPCTrainco.Umbraco.Extensions/Models/CheckoutCustomer.cs
@@ -128,7 +128,7 @@ namespace TPCTrainco.Umbraco.Extensions.Models
         public string BillPhoneExt { get; set; }
 
         [RequiredIfTrue("BillingDifferent", ErrorMessage = "Billing Email is required.")]
-        [RegularExpression(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$", ErrorMessage = "Email address is invalid.")]
+        [RegularExpression(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9][\-a-zA-Z0-9]{0,22}[a-zA-Z0-9]))$", ErrorMessage = "Email address is invalid.")]
         public string BillEmail { get; set; }
 
         [DisplayName("Payment Type")]
